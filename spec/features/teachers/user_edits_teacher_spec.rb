3f6152c5aef546aaac1917959391456e05f03447
Jan Mrowiec <jasiek92@gmail.com>
2015-10-18 17:22:54 +0200

    Specific teacher for creating subject_item in spec

--- spec/features/teachers/user_edits_teacher_spec.rb
+++ spec/features/teachers/user_edits_teacher_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 feature 'User edits teacher spec' do
   let!(:teacher) { create :teacher, first_name: 'Pies', last_name: 'Pluto', academic_title: 'Dr.' }
-  let!(:subject_item) { create :subject_item, title: 'History of NY' }
+  let!(:subject_item) { create :subject_item, title: 'History of NY', teacher: teacher }
 
   background do
     sign_in
@@ -31,6 +31,7 @@ feature 'User edits teacher spec' do
     visit teachers_path
     find(:xpath, "//a[@title='edit']").click
     find("input[type='checkbox']").set(true)
+
     click_button 'Update Teacher'
 
     visit report_subjects_path
